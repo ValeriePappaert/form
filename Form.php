db618661af99e7323f0768f2831d98b090cf777e
ValeriePappaert <valerie.pappaert@hotmail.com>
2018-05-15 06:15:34 +0200

    Added dropdown field

--- Form.php
+++ Form.php
@@ -10,9 +10,10 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 $name=trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
 $phone=trim(filter_input(INPUT_POST,"phone",FILTER_SANITIZE_STRING));
 $email=trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
+$subject=$_POST["subject"];
 $message=trim(filter_input(INPUT_POST,"message",FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-if($name=="" OR $phone=="" OR $email=="" OR $message==""){
-  $error_message = "Gelieve uw voornaam en naam, telefoonnummer, emailadres en bericht in de vereiste velden in te vullen.";
+if($name=="" OR $email=="" OR $subject =="" OR $message==""){
+  $error_message = "Gelieve uw voor- en familienaam, emailadres, onderwerp en bericht in de vereiste velden in te vullen.";
 }
 if (!isset($error_message) && $_POST["address"]!="") {
   $error_message = "Bad form input";
@@ -22,9 +23,10 @@ if(!isset($error_message) && !PHPMailer::validateAddress($email)) {
 }
 if (!isset($error_message)) {
     $email_body = "";
-    $email_body.= "Voornaam en naam: " . $name . "\n";
-    $email_body.= "Telefoonnummer: " . $phone . "\n";
+    $email_body.= "Voornaam en familienaam: " . $name . "\n";
     $email_body.= "Emailadres: " . $email . "\n";
+    $email_body.= "Telefoonnummer: " . $phone . "\n";
+    $email_body.= "Onderwerp: " . $subject . "\n";
     $email_body.= "Bericht: " . $message . "\n";
     $mail = new PHPMailer;
     //Tell PHPMailer to use SMTP
@@ -79,19 +81,26 @@ if (!isset($error_message)) {
       <p class="text-base align-center message_succes"> Bedankt voor je bericht! Ik neem spoedig contact met je op. </p><?php ;
     } else { ?>
       <div class="form align-center">
-        <form method = "POST" action="index.php#contact">
-          <label for="name">Voornaam en naam</label><br/>
+        <form method = "POST" action="contact.php#contact">
+          <label for="name">Voornaam en familienaam*</label><br/>
           <input type="text" id="name" name="name" value="<?php if (isset ($name)) echo $name; ?>"/><br/>
+          <label for="email">Email*</label><br/>
+          <input type="text" id="email" name="email" value="<?php if (isset ($email)) echo $email; ?>"/><br/>
           <label for="phone">Telefoonnummer</label><br/>
           <input type="text" id="phone" name="phone" value="<?php if (isset ($phone)) echo $phone; ?>"/><br/>
-          <label for="email">Emailadres</label><br/>
-          <input type="text" id="email" name="email" value="<?php if (isset ($email)) echo $email; ?>"/><br/>
-          <label for="message">Bericht</label><br/>
+          <label for="subject">Onderwerp*</label><br/>
+          <select id="subject" name="subject">
+            <option <?php if (isset($subject) && $subject=="-") echo "selected";?>>-</option>
+            <option <?php if (isset($subject) && $subject=="Vakantieopvang") echo "selected";?>>Vakantieopvang</option>
+            <option <?php if (isset($subject) && $subject=="Workshops") echo "selected";?>>Workshops</option>
+            <option <?php if (isset($subject) && $subject=="Andere") echo "selected";?>>Andere</option>
+          </select><br/>
+          <label for="message">Bericht*</label><br/>
           <textarea name="message" id="message" cols="60" rows="15" placeholder="Typ hier je bericht."><?php if (isset ($message)) echo $message; ?></textarea><br/>
           <label class="honey" for="address">Address</label>
           <input class="honey" type="text" name="address" id="address"/>
           <p class="honey">Gelieve dit veld leeg te laten.</p>
-          <input class="btn btn-pink align-right" type="submit" value="Verzenden"/>
+          <input class="btn btn-whitebg-red" type="submit" value="Verzenden"/>
         </form>
       </div><?php
     } ?>
